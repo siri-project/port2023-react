61b955540cdf1ffdcb40098aa91cd60a5f35ff8b
장은실_웹개발1팀 <esjang@eduwill.net>
2024-02-08 16:35:46 +0900

    component complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react"; // react library 호출
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import HomeView from "./views/HomeView";
 
 const App = () => { // App 상수 선언
-	return <div>App</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" elemen={<HomeView />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App; // App 컴포넌트 내보내기
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./assets/scss/style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root")); // createRoot() 메서드 사용하여 렌더링할 최상위(root) 컨테이너 생성
 root.render(
