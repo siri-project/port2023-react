32918ffe869679a6e9dfcad9efd34e8b0cbcbb11
장은실_웹개발1팀 <esjang@eduwill.net>
2024-02-21 16:31:30 +0900

    intro complete

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
+import about from "../assets/img/about.jpg";
+
+const introText = {
+	title: "port developer",
+	desc: ["This is", "my", "portfolio"]
+};
+
 const Intro = () => {
-	return <div>Intro</div>;
+	return (
+		<section id="intro">
+			<div className="intro-inner">
+				<h1 className="intro-title">{introText.title}</h1>
+				<div className="intro-lines" aria-hidden="true">
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+				</div>
+				<div className="intro-text">
+					<div className="text">
+						<div>{introText.desc[0]}</div>
+						<div>{introText.desc[1]}</div>
+						<div>{introText.desc[2]}</div>
+					</div>
+					<div className="img">
+						<img src={about} alt="" />
+					</div>
+				</div>
+				<div className="intro-lines bottom" aria-hidden="true">
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Intro;
