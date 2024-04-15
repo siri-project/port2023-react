53369af2cf84e760e12617eadf7d0b6fc253e587
siri-project <wdt0134@gmail.com>
2024-04-15 14:04:00 +0900

    contact complete

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 
+const contactText = [
+	{
+		link: "",
+		title: "KAKAO"
+	},
+	{
+		link: "mailto:wdt0134@gmail.com",
+		title: "wdt0134@gmail.com"
+	},
+];
+
 const Contact = () =>{
-	return <div>Contact</div>;
+	return (
+		<section id="contact">
+			<div className="contact-inner">
+				<h2 className="contact-title">Contact</h2>
+				<div className="contact-lines" aria-hidden="true">
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+				</div>
+				<div className="contact-text">
+					<div className="text">
+						{contactText.map((contact, key) >= (
+							<div key={key}>
+								<a href="{contact.link}" rel="noopener noreferrer" target="_blank">
+									{contact.title}
+								</a>
+							</div>
+						))}
+					</div>
+				</div>
+				<div className="contact-lines bottom" aria-hidden="true">
+					<span className="line"></span>
+					<span className="line"></span>
+					<span className="line"></span>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Contact;
